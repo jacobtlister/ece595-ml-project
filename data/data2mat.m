c18fe70dca6d1ab1773b23e3c18814f58f7cb508
jacobtlister <jacoblister52262@gmail.com>
2024-12-07 18:17:08 -0500

    added a description at the top of data2mat.m

--- data/data2mat.m
+++ data/data2mat.m
@@ -1,4 +1,12 @@
-
+% ---------------------------------------------------------------------------
+% Oliver Bartz
+% Jacob Lister
+% ECE 595 Term Project
+% MATLAB Data Formatting Script
+% Reads all of the chopped audio files in and creates a .mat file
+% containing all of the data for easy reading at the start of each run
+% of the main program.
+% ---------------------------------------------------------------------------
 
 clear
 clc
