e0acf4db2bfc92084ef167db20af6d516cc837b3
jacobtlister <jacoblister52262@gmail.com>
2024-12-07 18:54:13 -0500

    made it read the data right

--- ece595.m
+++ ece595.m
@@ -7,13 +7,11 @@ load("/data/audio_data")
 cv = cvpartition(size(clean_data,1), 'HoldOut', 0.2);
 idx = cv.test;
 
-dataTrain_c = clean_data(~idx, :);
-dataTest_c = clean_data(idx, :);
+dataTrain_c = clean_data(~idx);
+dataTest_c = clean_data(idx);
 
-dataTrain_n = noisy_data(~idx, :);
-dataTest_n = noisy_data(idx, :);
+dataTrain_n = noisy_data(~idx);
+dataTest_n = noisy_data(idx);
 
-dataTrain_b = backg_data(~idx, :);
-dataTest_b = backg_data(idx, :);
-
-
+dataTrain_b = backg_data(~idx);
+dataTest_b = backg_data(idx);
