9f9ff9c5c21c80f3604538351217877a7f888f05
jacobtlister <jacoblister52262@gmail.com>
2024-12-07 17:19:15 -0500

    renamed concatenated files and chopped them into 20ms chunks

---
+++ data/chopchop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# File: chopchop.sh
+
+: <<'info'
+    description
+        chops the audio files in /concatenated/ into 20ms chunks and puts them into their own folders.
+
+    required packages
+        ffmpeg
+
+    additional notes
+        EXTRA INFO IF NEEDED
+info
+
+# my goat
+# https://superuser.com/a/525217
+
+ffmpeg -i ./concatenated/concat_clean.wav -f segment -segment_time 0.02 -c copy ./segmented/clean/clean%03d.wav
+
+ffmpeg -i ./concatenated/concat_noisy.wav -f segment -segment_time 0.02 -c copy ./segmented/noisy/noisy%03d.wav
+
+ffmpeg -i ./concatenated/concat_noisy_background.wav -f segment -segment_time 0.02 -c copy ./segmented/noisy_background/noisy_background%03d.wav
